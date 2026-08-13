4b9d46fd414bce25fe90723d73d19a94dec8b99e
sunnygajula <sunnygajula112233@gmail.com>
2026-08-13 18:44:57 +0530

    Fixed CI/CD headless Chrome configuration

--- conftest.py
+++ conftest.py
@@ -1,16 +1,21 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="function")
 def setup(request):
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(10)
-    driver.maximize_window()
+    chrome_options = Options()
+    # Cloud Linux సర్వర్స్ లో బ్రౌజర్ బ్యాక్‌గ్రౌండ్‌లో రన్ అవ్వడానికి Headless ఆప్షన్స్
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
     
-    # Class-level tests కోసం driver ని పంపుతున్నాం
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(10)
+    
     request.cls.driver = driver
     
     yield driver
     
-    # Test పూర్తయ్యాక బ్రౌజర్ ఆటోమేటిక్‌గా క్లోజ్ అవుతుంది
     driver.quit()
