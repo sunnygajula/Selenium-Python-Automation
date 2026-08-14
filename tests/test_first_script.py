df7c5a4b6c04c2a32d292d6630ec4f9ec1e42ebf
sunnygajula <sunnygajula112233@gmail.com>
2026-08-14 10:43:37 +0530

    Fix: Added headless options to test_first_script.py

---
+++ tests/test_first_script.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def test_google_title():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://www.google.com")
+    assert "Google" in driver.title
+    driver.quit()
